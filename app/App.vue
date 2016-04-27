7771cc6faf4dba1022cb9d1acc85299ddfdc0b6f
SonyaMoisset <sonya.moisset@gmail.com>
2016-04-27 20:56:31 +0900

    Add app files

---
+++ app/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <sidebar
+      :categories="categories"
+      v-on:category-selected="setSelectedCategory">
+    </sidebar>
+    <bookmark-list
+      :bookmarks="bookmarks | filterByCategory selectedCategory"
+      :categories="categories">
+    </bookmark-list>
+  </div>
+</template>
+
+<script>
+  import store from './store'
+  import Sidebar from './components/Sidebar.vue'
+  import BookmarkList from './components/BookmarkList.vue'
+  import { filterByCategory } from './filters'
+
+  export default {
+
+    components: {
+      Sidebar,
+      BookmarkList
+    },
+
+    data () {
+      return {
+        categories: {},
+        bookmarks: {},
+        selectedCategory: ''
+      }
+    },
+
+    filters: {
+      filterByCategory
+    },
+
+    created () {
+      store.on('data-updated', this.updateListings)
+    },
+
+    methods: {
+      updateListings (categories, bookmarks) {
+        this.categories = categories
+        this.bookmarks = bookmarks
+      },
+
+      setSelectedCategory (category) {
+        this.selectedCategory = category;
+      }
+
+    }
+
+  }
+</script>
